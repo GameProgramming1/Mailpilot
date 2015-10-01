13bac102aba38587e0addd041d0092fc5961b7c9
Sukhpreet Sekhon <ersukhpreetsekhon03@gmail.com>
2015-10-01 13:25:38 -0400

    added audio

---
+++ Assets/_Scripts/PlayerCollider.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollider : MonoBehaviour {
+
+    //private instance variable
+    private AudioSource[] _audioSources; //array of audio sources
+    private AudioSource _CloudAudioSource;
+    private AudioSource _IslandAudioSource;
+
+
+	// Use this for initialization
+	void Start () {
+        this._audioSources = this.GetComponents<AudioSource> ();
+        this._CloudAudioSource = this._audioSources[1];//reference cloud sound
+	this._IslandAudioSource = this._audioSources[2];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D (Collider2D otherGameObject) 
+    {
+        if (otherGameObject.tag == "Island")
+        {
+            this._IslandAudioSource.Play();
+        }
+        else
+        {
+            this._CloudAudioSource.Play();
+        }
+    }
+}
